2369df5bc3aebc61c90e957db9e18b85006ec066
GauravJani <34713448+bitwisebro@users.noreply.github.com>
2024-12-21 19:19:48 +0530

    Minor: Add Assertions for Successful Registration

--- src/pages/register.page.ts
+++ src/pages/register.page.ts
@@ -3,22 +3,22 @@ import { expect, type Locator, type Page } from '@playwright/test';
 
 export default class RegisterPage {
     constructor(readonly page: Page) { }
-    private readonly emailField:Locator = this.page.getByPlaceholder('Email Address');
-    private readonly nextBtn:Locator = this.page.getByLabel('Next');
-    private readonly firstNameField:Locator = this.page.getByLabel('First Name');
-    private readonly lastNameField:Locator = this.page.getByLabel('Last Name');
-    private readonly mobileNumberField:Locator = this.page.locator("input[type='tel']");
-    private readonly passwordField:Locator = this.page.getByLabel('Create a Password');
-    private readonly submitBtn:Locator = this.page.getByLabel('Submit');
-    private readonly ageCriteriaCheckBox:Locator = this.page.getByLabel('I confirm that I am at least');
+    private readonly emailField: Locator = this.page.getByPlaceholder('Email Address');
+    private readonly nextBtn: Locator = this.page.getByLabel('Next');
+    private readonly firstNameField: Locator = this.page.getByLabel('First Name');
+    private readonly lastNameField: Locator = this.page.getByLabel('Last Name');
+    private readonly mobileNumberField: Locator = this.page.locator("input[type='tel']");
+    private readonly passwordField: Locator = this.page.getByLabel('Create a Password');
+    private readonly submitBtn: Locator = this.page.getByLabel('Submit');
+    private readonly ageCriteriaCheckBox: Locator = this.page.getByLabel('I confirm that I am at least');
 
 
-    async enterEmailAndClickNext(applicantEmail:string){
+    async enterEmailAndClickNext(applicantEmail: string) {
         await this.emailField.fill(applicantEmail);
         await this.nextBtn.click();
     }
 
-    async enterDetailsAndClickNext(firstName:string, lastName:string,mobileNumber:string, password:string){
+    async enterDetailsAndClickNext(firstName: string, lastName: string, mobileNumber: string, password: string) {
         await this.firstNameField.fill(firstName);
         await this.lastNameField.fill(lastName);
         await this.passwordField.fill(password);
@@ -27,7 +27,7 @@ export default class RegisterPage {
         await this.submitBtn.click();
     }
 
-    async checkAgeCriteria(){
+    async checkAgeCriteria() {
         await this.ageCriteriaCheckBox.check();
     }
 }
--- src/tests/applicationProcess.test.ts
+++ src/tests/applicationProcess.test.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 import { faker } from '@faker-js/faker';
 import RegisterPage from '../pages/register.page';
 
@@ -8,12 +8,16 @@ test('Kaleidoscope Applicant Application process', async ({ page }) => {
     let userEmail = faker.internet.email(
         { firstName: userFirstName, lastName: userLastName, provider: `mailinator.com` });
     let userPassword = `${userFirstName}@${userLastName}_123`;
-    let userPhone=faker.phone.number();
+    let userPhone = faker.phone.number({ style: 'national' });
 
     await test.step("Register a new User", async () => {
-        await page.goto('/login',{timeout:20000});
+        await page.goto('/login', { timeout: 20000 });
         const registerPage = new RegisterPage(page);
         await registerPage.enterEmailAndClickNext(userEmail);
-        await registerPage.enterDetailsAndClickNext(userFirstName, userLastName,userPhone, userPassword);
+        await registerPage.enterDetailsAndClickNext(userFirstName, userLastName, userPhone, userPassword);
+
+        await page.waitForURL("https://apply.mykaleidoscope.com/applicant/applications", { waitUntil: 'networkidle' });
+        const popup = page.getByRole('heading', { name: `Welcome back, ${userFirstName}!` })
+        await expect(popup).toBeVisible();
     })
 });
